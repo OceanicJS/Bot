62a470731f063e05ef39e89d9f6c92fe559ea948
Donovan Daniels <hewwo@yiff.rocks>
2023-06-04 11:36:00 -0500

    type checking? what's that?

--- src/events/messageUpdate.ts
+++ src/events/messageUpdate.ts
@@ -1,13 +1,13 @@
 import EncryptionHandler from "../util/EncryptionHandler.js";
 import { saveSnipe } from "../util/util.js";
 import type {
-    AnyTextChannelWithoutGroup,
+    AnyTextableChannel,
     Client,
     JSONMessage,
     Message,
     Uncached
 } from "oceanic.js";
 
-export default async function messageUpdateEvent(this: Client, message: Message<Uncached | AnyTextChannelWithoutGroup>, oldMessage: JSONMessage | null) {
+export default async function messageUpdateEvent(this: Client, message: Message<Uncached | AnyTextableChannel>, oldMessage: JSONMessage | null) {
     await saveSnipe(message.author, message.channelID, EncryptionHandler.encrypt(message.content), oldMessage ? EncryptionHandler.encrypt(oldMessage.content) : null, "edit");
 }
